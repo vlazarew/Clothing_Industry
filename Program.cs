5fe0877313a4905c6e8ada120618ef70624f4f0f
lexus <lexus@DESKTOP-09I91RH>
2019-04-05 18:37:01 +0300

    Переименовал форму

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Clothing_Industry
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RegistrationForm());
+            Application.Run(new AuthentificationForm());
         }
     }
 }
